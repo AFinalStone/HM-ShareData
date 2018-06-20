4c403100bb5eb366e4d257ef106b0fc7916f7b82
shiyaolei <shiyaolei@dafy.com>
2018-06-20 21:50:38 +0800

    添加IOUKindEnum枚举对象

---
+++ library/src/main/java/com/hm/iou/sharedata/model/IOUKindEnum.java
@@ -0,0 +1,31 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.hm.iou.sharedata.model;
+
+import java.io.Serializable;
+
+public enum IOUKindEnum implements Serializable {
+    ElecBorrowReceipt(1),
+    FunReceipt(3),
+    PlatformReceipt(5),
+    PaperBorrowerReceipt(7),
+    PaperReceiveReceipt(8),
+    ElecReceiveReceipt(9);
+
+    int value;
+
+    private IOUKindEnum(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
