978e4160a402863b46d27205038aebf6d609ae17
shilei <shiyaolei@dafy.com>
2018-09-06 14:19:01 +0800

    为UserExtendInfo添加新的属性

--- library/src/main/java/com/hm/iou/sharedata/UserManager.java
+++ library/src/main/java/com/hm/iou/sharedata/UserManager.java
@@ -261,4 +261,5 @@ public class UserManager {
         return mSpUser.edit().clear().commit();
     }
 
+
 }
--- library/src/main/java/com/hm/iou/sharedata/model/UserExtendInfo.java
+++ library/src/main/java/com/hm/iou/sharedata/model/UserExtendInfo.java
@@ -7,11 +7,21 @@ public class UserExtendInfo {
 
     private UserThirdPlatformInfo thirdPlatformInfo;//用户绑定的第三方平台信息
 
+    private boolean haveOpenDebtList;//是否打开过记债本列表
+
     public UserThirdPlatformInfo getThirdPlatformInfo() {
         return thirdPlatformInfo;
     }
 
     public void setThirdPlatformInfo(UserThirdPlatformInfo thirdPlatformInfo) {
         this.thirdPlatformInfo = thirdPlatformInfo;
+    }
+
+    public boolean getHaveOpenDebtList() {
+        return haveOpenDebtList;
+    }
+
+    public void setHaveOpenDebtList(boolean haveOpenDebtList) {
+        this.haveOpenDebtList = haveOpenDebtList;
     }
 }
