e96d5a98841d616bc6c9f13d0b46786f904269f5
石磊 <syl@shileideMacBook-Pro.local>
2018-11-01 16:22:07 +0800

    添加重新进行活体校验的Event

---
+++ library/src/main/java/com/hm/iou/sharedata/event/FaceCheckAgainEvent.java
@@ -0,0 +1,10 @@
+package com.hm.iou.sharedata.event;
+
+/**
+ * 再次进行活体校验
+ *
+ * @author syl
+ * @time 2018/5/24 下午6:45
+ */
+public class FaceCheckAgainEvent {
+}
--- library/src/main/java/com/hm/iou/sharedata/event/RealNameEvent.java
+++ library/src/main/java/com/hm/iou/sharedata/event/RealNameEvent.java
@@ -1,9 +1,10 @@
 package com.hm.iou.sharedata.event;
 
 /**
- * Created by hjy on 2018/7/4.
+ * 再次进行实名认证
+ * @author syl
+ * @time 2018/10/31 下午4:33
  */
-
 public class RealNameEvent {
 
     public String tag;
