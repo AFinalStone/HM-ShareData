7d5d96ff1aac31ef382ee8e83a222556145f5215
石磊 <>
2019-09-20 10:18:45 +0800

    添加IOUStatusEnum枚举对象

---
+++ library/src/main/java/com/hm/iou/sharedata/dict/IOUStatusEnum.kt
@@ -0,0 +1,15 @@
+package com.hm.iou.sharedata.dict
+
+enum class IOUStatusEnum(val value: Int) {
+
+    Draft(0),   //草稿
+    Offical(2),  //正常的
+    Del(3), //删除或隐藏的
+    Close(4), //关闭
+    Finish(5), //已结清
+    WaitConfirm(12), //等待确认
+    TimeoutUnSign(13), //超时未签
+    JustComplete(14), //刚完成
+    Refused(15); //被拒绝
+
+}
