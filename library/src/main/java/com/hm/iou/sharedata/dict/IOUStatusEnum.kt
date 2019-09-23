9b42463ced4621b86de6f939d0cdeaf2c5dbb58c
houjinyun <houjinyun@gmail.com>
2019-09-23 12:31:44 +0800

    fix: 草稿状态错误

--- library/src/main/java/com/hm/iou/sharedata/dict/IOUStatusEnum.kt
+++ library/src/main/java/com/hm/iou/sharedata/dict/IOUStatusEnum.kt
@@ -2,7 +2,7 @@ package com.hm.iou.sharedata.dict
 
 enum class IOUStatusEnum(val value: Int) {
 
-    Draft(0),   //草稿
+    Draft(1),   //草稿
     Offical(2),  //正常的
     Del(3), //删除或隐藏的
     Close(4), //关闭
