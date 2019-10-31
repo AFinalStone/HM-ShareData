c4e7b49bc61764df002eac8623e2ced538f38171
houjinyun <houjinyun@gmail.com>
2019-10-31 19:05:18 +0800

    add: 更新借款详情事件

---
+++ library/src/main/java/com/hm/iou/sharedata/event/UpdateBorrowSquareEvent.kt
@@ -0,0 +1,13 @@
+package com.hm.iou.sharedata.event
+
+/**
+ * Created by hjy on 2019/10/31
+ *
+ * 更新求借广场事件
+ */
+class UpdateBorrowSquareEvent {
+
+    var applyId: String? = null
+    var contentId: String? = null
+
+}
