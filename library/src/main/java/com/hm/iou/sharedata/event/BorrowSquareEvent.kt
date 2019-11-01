865026122e2614805e674c4af0df8746c456c289
shilei <yaoleishi@gmail.com>
2019-11-01 10:29:28 +0800

    update: 修改求借广场通用eventbus事件的名称

---
+++ library/src/main/java/com/hm/iou/sharedata/event/BorrowSquareEvent.kt
@@ -0,0 +1,20 @@
+package com.hm.iou.sharedata.event
+
+/**
+ * Created by hjy on 2019/10/31
+ *
+ * 关闭求借广场页面事件
+ */
+class BorrowSquareEvent {
+    /**
+     * 业务通知方自定定义该业务事件的关键字
+     */
+    var key: String? = null
+
+    /**
+     * 业务通知事件所带的额外内容
+     */
+    var content: String? = null
+
+
+}
